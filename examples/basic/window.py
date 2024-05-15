9c5b36921a63006dd09c9d704d87ed9f10ecc1f2
Niklas Henning <niklashenningbusiness@gmail.com>
2024-05-16 00:37:44 +0200

    Update basic example

--- examples/basic/window.py
+++ examples/basic/window.py
@@ -1,5 +1,5 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QMainWindow, QLabel
+from PyQt6.QtWidgets import QMainWindow, QFormLayout, QWidget
 from src.pyqthotkey import HotkeyPicker
 
 
@@ -8,8 +8,9 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__(parent=None)
 
+        # Window settings
         self.setWindowTitle('Hotkey Picker Demo')
-        self.setFixedSize(360, 150)
+        self.resize(360, 150)
 
         # Define variables to store selected hotkeys in
         self.selected_hotkey_1 = 0
@@ -17,25 +18,30 @@ class Window(QMainWindow):
         self.selected_hotkey_2 = 0
         self.selected_hotkey_2_name = ''
 
-        # Add labels for the hotkey pickers
-        self.label_1 = QLabel(self)
-        self.label_1.setText('Hotkey 1:')
-        self.label_1.move(80, 32)  # Using fixed positions for simplicity
-
-        self.label_2 = QLabel(self)
-        self.label_2.setText('Hotkey 2:')
-        self.label_2.move(80, 78)
-
-        # Add first hotkey picker with default settings
+        # Add first hotkey picker
         self.hotkey_picker_1 = HotkeyPicker(self)
-        self.hotkey_picker_1.move(170, 32)
         self.hotkey_picker_1.hotkeyChanged.connect(self.hotkey_picker_1_changed)
         self.hotkey_picker_1.setHotkey(Qt.Key.Key_F5)
 
-        # Add second hotkey picker with default settings
+        # Add second hotkey picker
         self.hotkey_picker_2 = HotkeyPicker(self)
-        self.hotkey_picker_2.move(170, 78)
         self.hotkey_picker_2.hotkeyChanged.connect(self.hotkey_picker_2_changed)
+
+        # Set size constraints
+        self.hotkey_picker_1.setFixedHeight(32)
+        self.hotkey_picker_2.setFixedHeight(32)
+
+        # Form layout
+        form_layout = QFormLayout()
+        form_layout.addRow('Hotkey 1:', self.hotkey_picker_1)
+        form_layout.addRow('Hotkey 2:', self.hotkey_picker_2)
+        form_layout.setSpacing(25)
+        form_layout.setContentsMargins(25, 25, 25, 25)
+
+        # Set layout
+        central_widget = QWidget()
+        central_widget.setLayout(form_layout)
+        self.setCentralWidget(central_widget)
 
     def hotkey_picker_1_changed(self, key, key_name):
         # Handle change of hotkey 1
